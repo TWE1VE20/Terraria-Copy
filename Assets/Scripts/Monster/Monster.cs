4953011097a8596ef3af9d4c6ccf1aa8b3dfc0c9
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-03-15 20:55:20 +0900

    몬스터 구현중

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class Monster
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    protected int Hp;
+    
+    public Monster() { }
 }
