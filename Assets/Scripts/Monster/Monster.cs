77efd72428130f0a7187a357b6e346fb0a513daf
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-03-16 17:49:28 +0900

    몬스터 구현중 : 좀비 구현중, 중력 변경

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
-public class Monster
+
+[CreateAssetMenu(fileName = "Enemy Data", menuName = "Data/Enemy")]
+public class Monster : ScriptableObject
 {
-    protected int Hp;
-    
-    public Monster() { }
+    [Header("Status")]
+    public int id;
+    public string monsterName;
+    public int Hp;
+    public int Atk;
+    public int Dex;
+    public float knockBack;
+    public int DropMoney;
+
+    /*
+    if knockBack > 8.0 then knockBack = 8 + (knockBack - 8.0) * 0.9
+    if knockBack > 10.0 then knockBack = 10 + (knockBack - 10.0) * 0.8
+    if knockBack > 12.0 then knockBack = 12 + (knockBack - 12.0) * 0.7
+    if knockBack > 14.0 then knockBack = 14 + (knockBack - 14.0) * 0.6
+    if knockBack > 16.0 then knockBack = 16
+    if isCrit then knockBack = knockBack * 1.4
+    */
 }
--- Assets/Scripts/Monster/Zombie.cs
+++ Assets/Scripts/Monster/Zombie.cs
@@ -1,11 +1,213 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Zombie : Monster
+public class Zombie : MonoBehaviour
 {
-    public Zombie()
+    [Header("Component")]
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] Monster monsterStat;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer ZombieSprite;
+
+    [Header("Property")]
+    [SerializeField] float movePower;
+    [SerializeField] float breakPower;
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public float maxXSpeed;
+
+    [Header("Ground Check")]
+    [SerializeField] public LayerMask groundLayer;
+
+
+    private StateMachine fsm;
+    private Transform playerTransform;
+
+    private Collider2D groundCollider;  // 바닥 체크 콜라이더
+
+    public int hp { get; private set; }
+    public bool isGrounded { get; private set; }
+
+
+    private void Start()
     {
-        Hp = 100;
+        hp = monsterStat.Hp;
+        if(monsterStat.id == 3)
+        {
+            // animator의 layer 조정
+        }
+
+        fsm = new StateMachine();
+        fsm.Init(this);
+
+        groundCollider = GetComponent<Collider2D>();
+
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
+    private void Update()
+    {
+        fsm.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = CheckGround();
+    }
+
+    public void TakeHit(int damage)
+    {
+        hp -= damage;
+    }
+
+    bool CheckGround()
+    {
+        Vector2 rayOrigin = groundCollider.bounds.center;
+        Vector2 rayDirection = Vector2.down;
+        float rayDistance = groundCollider.bounds.extents.y;
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance, groundLayer);
+
+        if (hit.collider != null)
+            return true;
+        else
+            return false;
+    }
+
+    #region State
+    public enum State { Idle, Trace, Return, Die, Size }
+    private class StateMachine
+    {
+        private State curState;
+        private BaseState[] states;
+
+        public void Init(Zombie owner)
+        {
+            states = new BaseState[(int)State.Size];
+
+            states[(int)State.Idle] = new IdleState(this, owner);
+            states[(int)State.Trace] = new TraceState(this, owner);
+            states[(int)State.Return] = new ReturnState(this, owner);
+            states[(int)State.Die] = new DieState(this, owner);
+
+            curState = State.Idle;
+            states[(int)curState].Enter();
+        }
+
+        public void Update()
+        {
+            states[(int)curState].Update();
+            states[(int)curState].Transition();
+        }
+
+        public void ChangeState(State nextState)
+        {
+            states[(int)curState].Exit();
+            curState = nextState;
+            states[(int)curState].Enter();
+        }
+    }
+
+    private class BaseState
+    {
+        protected StateMachine fsm;
+        protected Zombie owner;
+
+        public BaseState(StateMachine fsm, Zombie owner)
+        {
+            this.fsm = fsm;
+            this.owner = owner;
+        }
+
+        public virtual void Enter() { }
+        public virtual void Update() { }
+        public virtual void Transition() { }
+        public virtual void Exit() { }
+    }
+
+    private class IdleState : BaseState
+    {
+        public IdleState(StateMachine fsm, Zombie owner) : base(fsm, owner)
+        {
+        }
+
+        public override void Transition()
+        {
+            if (owner.hp <= 0)
+            {
+                fsm.ChangeState(State.Die);
+            }
+            fsm.ChangeState(State.Trace);
+        }
+    }
+
+    private class TraceState : BaseState
+    {
+        public TraceState(StateMachine fsm, Zombie owner) : base(fsm, owner) { }
+
+        public override void Update()
+        {
+            Vector2 dir = (owner.playerTransform.position - owner.transform.position).normalized;
+            if (dir.x < 0 && owner.rigid.velocity.x > -owner.maxXSpeed)
+            {
+                owner.ZombieSprite.flipX = false;
+                owner.rigid.AddForce(Vector2.right * dir.x * owner.movePower);
+                owner.animator.SetBool("Move", true);
+            }
+            else if (dir.x > 0 && owner.rigid.velocity.x < owner.maxXSpeed)
+            {
+                owner.ZombieSprite.flipX = true;
+                owner.rigid.AddForce(Vector2.right * dir.x * owner.movePower);
+                owner.animator.SetBool("Move", true);
+            }
+            else
+            {
+                owner.animator.SetBool("Move", false);
+            }
+        }
+
+        public override void Transition()
+        {
+            if (owner.hp <= 0)
+            {
+                fsm.ChangeState(State.Die);
+            }
+        }
+    }
+
+    private class ReturnState : BaseState
+    {
+        public ReturnState(StateMachine fsm, Zombie owner) : base(fsm, owner)
+        {
+        }
+
+        public override void Transition()
+        {
+            if (owner.hp <= 0)
+            {
+                fsm.ChangeState(State.Die);
+            }
+        }
+    }
+
+    private class DieState : BaseState
+    {
+        private Coroutine routine;
+
+        public DieState(StateMachine fsm, Zombie owner) : base(fsm, owner)
+        {
+        }
+
+        public override void Enter()
+        {
+            routine = owner.StartCoroutine(DieRoutine());
+        }
+
+        IEnumerator DieRoutine()
+        {
+            yield return new WaitForSeconds(1);
+            Destroy(owner.gameObject);
+        }
+
+    }
+
+    #endregion
 }
